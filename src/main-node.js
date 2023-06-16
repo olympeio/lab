f9184262fde86ff0863d69b5431751ac883f2424
Poseidon <poseidon@olympe.io>
2023-06-16 10:33:33 +0200

    Adding missing libraries

--- src/main-node.js
+++ src/main-node.js
@@ -2,6 +2,9 @@
 // Import Olympe runtime or DRAW
 import 'olympe';
 
+import '@olympeio/core';
+import '@olympeio-extensions/commons';
+
 // Import project bricks (we use webpack-import-glob-loader to import all bricks)
 import './common/**/*.js';
 import './node/**/*.js';
--- src/main-web.js
+++ src/main-web.js
@@ -1,6 +1,9 @@
 // Import Olympe runtime or DRAW
 import 'olympe';
 
+import '@olympeio/core';
+import '@olympeio-extensions/commons';
+
 // Import project bricks (we use webpack-import-glob-loader to import all bricks)
 import './common/**/*.js';
 import './web/**/*.js';
