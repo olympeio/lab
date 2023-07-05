db3e6bb0d20474a6951fa6387d5c4a793cb16f10
Syméon del Marmol <symeon.delmarmol@olympe.ch>
2023-07-05 15:19:12 +0200

    Clean up structure and add proper readme

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-An olympe extension library.
+## Olympe LAB Extension
 
-You can build the library and reference it in npm using:
-```
-npm run build
-cd dist
-npm link
-```
+This project is made for the Olympe community to be able to create coded bricks (logic and visual) and deploy them to the community environment.
+
+### How to participate
+
+1. First register to the community following the instructions on the [support website](https://olympe.support/#getting-started).
+2. Code your bricks and test them [locally](https://olympe.support/local_install). 
+3. Create a pull request with your code:
+   1. clone this repository
+   2. create a branch based on the `develop` branch. Name it like this: `<community username>-<bricks set name>`
+   3. create a pull request of your branch to the develop branch. Ensure that you respect the structure specified in the [rules](#Rules).
+
+### Rules
+
+This is a public repository, so all the code you will push will be publicly visible. This also means that you will share `src` folder with others.
+
+Here are the rules and the folder structure you need to follow in order to have accepted pull requests:
+
+1. Nothing outside the `src` folder will be accepted.
+2. Inside the `src` folder, you will see 3 folders:
+   1. `common`: contain all files added to both Browser and NodeJS bundles. It MUST be code that works on both environments.
+   2. `web`: contain all files added ONLY to the Browser bundle. It will typically contain the visual components.
+   3. `node`: contain all files added ONLY to the NodeJS bundle. It will typically contain logic bricks that use backend libraries.
+3. Inside the 3 sub-folders, you must put your files in a folder named using your GitHub username. We won't accept any change coming from a user that update files in another folder than its own. 
+   - Example: my username on GitHub is `john.smith`, I'll add my visual component js files into `src/web/john.smith` folder. 
+4. All source files are written in javascript or typescript.
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const {IgnorePlugin} = require('webpack');
 const Copy = require('copy-webpack-plugin');
 const GenerateJsonPlugin = require('generate-json-webpack-plugin');
-const {merge} = require('webpack-merge');
-const runtimeNodePath = path.resolve(__dirname, 'node_modules/@olympeio/runtime-node');
-const olympeioPath = path.resolve(__dirname, 'node_modules/@olympeio');
-const olympeioExtensionsPath = path.resolve(__dirname, 'node_modules/@olympeio-extensions');
-
+const nodeExternals = require('webpack-node-externals');
 
 const dist = path.join(__dirname, 'dist');
 const npmPackage = require('./package.json');
-const drawPath = path.resolve(__dirname, 'node_modules/@olympeio/draw');
+const runtimePath = path.resolve(__dirname, 'node_modules/@olympeio/runtime-web');
 
-const commonConfig = {
+module.exports = {
+    entry: {
+        web: './src/main-web.js',
+        node: './src/main-node.js'
+    },
     output: {
+        filename: 'main-[name].js',
         path: dist,
         globalObject: 'this',
         library: npmPackage.name,
@@ -23,129 +23,41 @@ const commonConfig = {
     mode: 'development',
     devtool: 'source-map',
     resolve: {
+        extensions: ['.js', 'jsx', '.tsx', '.ts'],
         alias: {
-            olympe: drawPath
+            olympe: runtimePath
         }
     },
     module: {
         rules: [
-            {test: /\.js$/, enforce: 'pre', use: 'source-map-loader'},
-            {test: /\.js$/, enforce: 'pre', use: 'webpack-import-glob-loader'},
+            {test: /\.(tsx?)$/, use: 'ts-loader', exclude: /node_modules/},
+            {test: /\.(jsx)$/, exclude: /node_modules/, loader: 'babel-loader', options: { presets: ["@babel/env", "@babel/react"] }},
+            {test: /\.js|ts$/, enforce: 'pre', use: 'source-map-loader'},
+            {test: /\.js|ts$/, enforce: 'pre', use: 'webpack-import-glob-loader'},
             {test: /\.css$/, use: ['style-loader', 'css-loader']},
         ]
     },
+    externals: ['olympe', nodeExternals()],
     plugins: [
         new CleanWebpackPlugin(),
-        new Copy({
-            patterns: [
-                {from: drawPath + '/version.json', to: 'version.json'}
-            ]
-        }),
-        new IgnorePlugin({resourceRegExp: /^(mssql*|mariasql|.oracle|oracledb|mysql|mysql2|mssql.|tedious|sqlite3|better-sqlite3|@vscode\/sqlite3|pg-query-stream|pg-native|node-pre-gyp)$/}),
-    ],
-    ignoreWarnings: [{message: /Empty results for "import '\.\/bricks\/\*\*\/\*\.js'"/}]
-};
-
-const plugins = {
-    plugins: [
-        new CleanWebpackPlugin({
-            cleanOnceBeforeBuildPatterns: ['**/*', '!main*']
-        }),
         new GenerateJsonPlugin(
             'package.json',
             {
                 name: npmPackage.name,
                 version: npmPackage.version,
+                files: npmPackage.files,
                 main: 'main-node.js',
                 browser: 'main-web.js',
                 dependencies: npmPackage.dependencies,
-                dcInitConfig: 'import/dcInitConfig.json'
+                dcInitConfig: 'import/dcInitConfig.json',
+                homepage: 'https://github.com/olympeio/lab',
             }
         ),
         new Copy({
             patterns: [
-                {from: '.dc-init', to: 'import', globOptions: {ignore: ['**/id_rsa']}}
+                {from: '.dc-init', to: 'import', globOptions: {ignore: ['**/id_rsa']}},
+                {from: 'README.md', to: 'README.md'}
             ]
         }),
-    ]
-};
-
-const server = {
-    devServer: {
-        port: 8888,
-        client: {
-            overlay: {
-                warnings: false
-            }
-        },
-        static: {
-            directory: path.join(__dirname, 'dist/server')
-        },
-        devMiddleware: {
-            writeToDisk: true
-        }
-    }
-};
-
-const draw = {
-    name: 'draw',
-    output: {path: path.join(__dirname, 'dist/draw'), globalObject: 'this'},
-    entry: './src/main-web.js',
-    resolve: {
-        alias: {olympe: drawPath}
-    },
-    plugins: [
-        new Copy({
-            patterns: [
-                {from: drawPath + '/index.html', to: 'index.html'},
-                {from: drawPath + '/images', to: 'images'},
-                {from: drawPath + '/fonts', to: 'fonts'},
-                {from: drawPath + '/css', to: 'css'},
-                {from: drawPath + '/doc', to: 'doc'},
-                {from: 'res'},
-                {from: drawPath + '/maintenance', to: 'maintenance'},
-            ]
-        })
-    ]
-};
-
-const node = {
-    name: 'node',
-    target: 'node',
-    entry: './src/main-node.js',
-    output: {filename: 'main-node.js'},
-    externals: ['os']
-};
-
-const nodeAI = {
-    name: 'nodeai',
-    target: 'node',
-    entry: './src/main-node.js',
-    output: {path: path.join(__dirname, 'dist/node'), globalObject: 'this', filename: 'main.js'},
-    plugins: [
-        new Copy({
-            patterns: [{
-                from: path.resolve(__dirname, 'res/oConfigNode-local.json'), to: 'oConfig.json'
-            }
-            ]
-        })
     ],
-    node: {
-        __dirname: false,
-    },
-
-    externals: ['os'],
-    resolve: {
-        alias: {
-            olympe: runtimeNodePath,
-            '@olympeio': olympeioPath,
-            '@olympeio-extensions': olympeioExtensionsPath
-        }
-    },
-
 };
-
-module.exports = [
-    merge(commonConfig, node, plugins),
-    merge(commonConfig, server, draw),
-    merge(commonConfig, nodeAI, plugins)];
